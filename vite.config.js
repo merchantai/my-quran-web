4c2f90411beff3ca015f9041f5f924b992dd5ef3
Merchant Ashraf Iqbal <merchantai@gmail.com>
2025-07-27 16:31:38 +0530

    updated base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/my-quran-web/',
+  base: '/merchantai/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
