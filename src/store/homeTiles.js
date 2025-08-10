3030825c13ec8eb90e07f44e7c37981b370f6ed0
Merchant Ashraf Iqbal <merchantai@gmail.com>
2025-08-10 14:33:00 +0530

    more updates and improvements

---
+++ src/store/homeTiles.js
@@ -0,0 +1,32 @@
+export default [
+      {
+        title: 'Resume',
+        icon: 'book.png',
+        route: 'NextPage',
+      },
+      {
+        title: 'Goto Page No',
+        icon: 'goto.png',
+        route: 'NextPage',
+      },
+      {
+        title: 'GoTo Surah',
+        icon: 'goto.png',
+        route: 'NextPage',
+      },
+      {
+        title: 'GoTo Juz',
+        icon: 'goto.png',
+        route: 'NextPage',
+      },
+      {
+        title: 'Bookmarks',
+        icon: 'bookmark-white.png',
+        route: 'BookmarksPage',
+      },
+      {
+        title: 'Settings',
+        icon: 'settings.png',
+        route: 'SettingsPage',
+      },
+    ]
